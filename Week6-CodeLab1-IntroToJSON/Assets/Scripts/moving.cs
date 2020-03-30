4fea422b19ce5b46bc9cc2f0993e57b469c81d28
yichenljing <40904846+yichenljing@users.noreply.github.com>
2020-03-30 12:40:08 -0400

    moving floor

---
+++ Week6-CodeLab1-IntroToJSON/Assets/Scripts/moving.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moving : MonoBehaviour {
+    public float start_x;
+    public float start_y;
+    public float end_x;
+    public float end_y;
+
+    public float speed=0.01f;
+    Vector3 startposition;
+    Vector3 endposition;
+    Vector3 destination;
+    void Awake() {
+        startposition = new Vector3(transform.localPosition.x + start_x, transform.localPosition.y + start_y, transform.localPosition.z); // to adjust the distance
+        endposition = new Vector3(transform.localPosition.x + end_x, transform.localPosition.y + end_y, transform.localPosition.z);
+        destination = endposition;
+    }
+	void Update () {
+
+        if (Vector3.Distance(transform.localPosition, endposition)<0.1f){
+            destination = startposition;          
+        }else if (Vector3.Distance(transform.localPosition, startposition) < 0.1f)
+        {
+            destination = endposition;
+        }
+
+        transform.localPosition = Vector3.MoveTowards(transform.localPosition, destination, speed);
+    }
+}
